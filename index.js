e393ad52acb5ea3c9156dc82cb31c2897f707edf
BozeGame <39629558+BozeGame@users.noreply.github.com>
2018-05-25 18:30:34 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("/")
+
+bot.on('ready', function() {
+    bot.user.setUsername("GuildeRP")
+    bot.user.setPresence({ game: { name: 'Préparer les Requêtes'}, status: 'online'}) //en ligne;
+    console.log("Connected")
+});
+
+bot.login(process.env.TOKEN);
+
+
+bot.on('message', message => {
+    if (message.content === "TEST"){
+        message.reply("test")
+    }
+}
+)
