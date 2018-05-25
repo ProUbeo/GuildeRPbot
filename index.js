9f16a0fed99677324e62059cd00955df257b95c9
BozeGame <39629558+BozeGame@users.noreply.github.com>
2018-05-25 18:57:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
     if (message.content === "TEST"){
-        message.send("TEST")
+        message.channel.send("TEST")
     }
 }
 )
