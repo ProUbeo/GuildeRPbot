909a6806f989c06f2bb85091f73fa93256148bfb
BozeGame <39629558+BozeGame@users.noreply.github.com>
2018-05-26 14:57:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
-
+     if(message.content.startsWith("/destructor")){
+        message.channel.send(`**${message.author.username}** lance la capacité de dark sur **${message.username}** !\nDégâts : 200 \nDégâts sur utilisateur : 100`,{
+        file: "http://img.maxisciences.com/article/planete/que-se-passera-t-il-si-la-terre-s-arrete-de-tourner_7615fcfc450f978e6ad0da439cd7a69e419b5846.jpg"
+    })
 }
 )
