61b56e0dbbe664387664a29fa90bd4b01bdc3f24
BozeGame <39629558+BozeGame@users.noreply.github.com>
2018-05-26 15:53:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,14 @@ bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
      if(message.content.startsWith("/destructor")){
-        message.channel.send(`**${message.author.username}** lance destructor !\nDégâts : 200 \nDégâts sur utilisateur : 100`,{
+        message.channel.send(`**${message.author.username}** lance destructor (type dark) !\nDégâts : 200 \nDégâts sur utilisateur : 100`,{
         file: "http://img.maxisciences.com/article/planete/que-se-passera-t-il-si-la-terre-s-arrete-de-tourner_7615fcfc450f978e6ad0da439cd7a69e419b5846.jpg"
     })
+}
+if(message.content.startsWith("/tempete solaire")){
+    message.channel.send(`**${message.author.username}** lance destructor !\nDégâts : 200 \nDégâts sur utilisateur : 100`,{
+    file: "http://img.maxisciences.com/article/planete/que-se-passera-t-il-si-la-terre-s-arrete-de-tourner_7615fcfc450f978e6ad0da439cd7a69e419b5846.jpg"
+})
 }
 }
 )
