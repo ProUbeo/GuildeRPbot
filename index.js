a927b583d8622593c250f06cdfcedc1d94baf303
BozeGame <39629558+BozeGame@users.noreply.github.com>
2018-05-27 10:19:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
-     if(message.content.startsWith("/destructor **{MessageMentions.members}**")){
+     if(message.content.startsWith("/destructor **${MessageMentions.members}**")){
         message.channel.send(`**${message.author.username}** lance destructor (type dark) !\nDégâts : 200 \nDégâts sur utilisateur : 100`,{
         file: "http://img.maxisciences.com/article/planete/que-se-passera-t-il-si-la-terre-s-arrete-de-tourner_7615fcfc450f978e6ad0da439cd7a69e419b5846.jpg"
     })
